367d0e90a2f1772bca1c7e8e851294b68469152b
straightupjac <jdc.jaclyn@gmail.com>
2022-08-21 14:13:00 -0700

    init chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,55 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+/* Theming */
+const theme = extendTheme({
+  components: {
+    Button: {
+      baseStyle: {
+        fontFamily: "Apple Garamond, serif",
+        fontWeight: 'normal',
+      },
+    },
+    Input: {
+      baseStyle: {
+        borderColor: '#7B622B'
+      }
+    }
+  },
+  styles: {
+    global: {
+      a: {
+        color: '#7B622B',
+        _hover: {
+          textDecoration: 'underline',
+        },
+      },
+      h1: {
+        fontSize: '4xl',
+        fontWeight: 'bold',
+      },
+      h2: {
+        fontSize: '2xl',
+        fontWeight: 'bold',
+      },
+      h3: {
+        fontSize: 'lg'
+      },
+      h4: {
+        fontSize: 'md'
+      },
+    },
+  },
+})
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
-export default MyApp
+export default App
