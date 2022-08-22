1f47c31d3f4fbaa8ffcd2a9bac7bc5c033129762
amirgamil <amir.amgad01@gmail.com>
2022-08-21 23:34:33 -0700

    chore: add formatted merkle tree

--- utils/zkp.ts
+++ utils/zkp.ts
@@ -1,5 +1,5 @@
 import MerkleTree from "fixed-merkle-tree";
-import { mimcHash } from "./mimc";
+import { mimcHash, mimcSponge, modPBigInt } from "./mimc";
 
 export function bigIntToArray(n: number, k: number, x: bigint) {
     let divisor = BigInt(1);
@@ -18,28 +18,36 @@ export function bigIntToArray(n: number, k: number, x: bigint) {
 
 // taken from generation code in dizkus-circuits tests
 export function pubkeyToXYArrays(pk: string) {
-    const XArr = bigIntToArray(64, 4, BigInt("0x" + pk.slice(4, 4 + 64))).map((el) => el.toString());
-    const YArr = bigIntToArray(64, 4, BigInt("0x" + pk.slice(68, 68 + 64))).map((el) => el.toString());
+    const XArr = bigIntToArray(64, 4, BigInt("0x" + pk.slice(4, 4 + 64))).map(
+        (el) => el.toString()
+    );
+    const YArr = bigIntToArray(64, 4, BigInt("0x" + pk.slice(68, 68 + 64))).map(
+        (el) => el.toString()
+    );
 
     return [XArr, YArr];
 }
 
+//input: list of public keys stored as hex
 //merkle tree of list of "sybil-proof" public keys - leaves are the Mimc hash of Eddsa public keys (split into an array of
 //2 elements to be compatible with construction in the snark), so we need to convert leafPublicKeys into that
-export function createMerkleTree(currReaderAddr: string, leafPublicKeys: string[]) {
+export function createMerkleTree(
+    currReaderAddr: string,
+    leafPublicKeys: string[]
+) {
     //Yes the @ts-ignores are not ideal but unfortunately they're necessary because
     //the type definitions do not support bigints
     //@ts-ignore
 
-    const formattedHashedAddrs: string[] = [];
+    const formattedHashedAddrs: bigint[] = [];
     for (const pubKey of leafPublicKeys) {
-        //do stuff
+        formattedHashedAddrs.push(BigInt(formatHexPubKey(pubKey)));
     }
 
     const tree = new MerkleTree(
         30,
         //@ts-ignore
-        leafAddrs.map((el) => BigInt(el)),
+        formattedHashedAddrs,
         //@ts-ignore
         { hashFunction: mimcHash(123) }
     );
@@ -50,7 +58,25 @@ export function createMerkleTree(currReaderAddr: string, leafPublicKeys: string[
 }
 
 export function sigToRSArrays(sig: string) {
-    const rArr = bigIntToArray(64, 4, BigInt("0x" + sig.slice(2, 2 + 64))).map((el) => el.toString());
-    const sArr = bigIntToArray(64, 4, BigInt("0x" + sig.slice(66, 66 + 64))).map((el) => el.toString());
+    const rArr = bigIntToArray(64, 4, BigInt("0x" + sig.slice(2, 2 + 64))).map(
+        (el) => el.toString()
+    );
+    const sArr = bigIntToArray(
+        64,
+        4,
+        BigInt("0x" + sig.slice(66, 66 + 64))
+    ).map((el) => el.toString());
     return [rArr, sArr];
 }
+
+//given hex public key, converts it to two element array, hashes those with Mimc (to match hashing format)
+const formatHexPubKey = (hexPubKey: string) => {
+    //assumes hexPubKey stored with 0x prefic
+    const arrPubKey = bigIntToArray(32, 2, BigInt(hexPubKey));
+    return mimcSponge(
+        arrPubKey.map((el) => modPBigInt(Number(el))),
+        1,
+        220,
+        123
+    )[0].toString();
+};
