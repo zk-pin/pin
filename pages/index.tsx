929076831577aaf9ba7aab5b98099f46dfe3dacf
amirgamil <amir.amgad01@gmail.com>
2022-08-21 22:10:41 -0700

    feat: add .prettier config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,37 @@
-import type { GetStaticProps, NextPage } from 'next'
-import styles from '@styles/Home.module.css'
-import prisma from '@utils/prisma';
-import { CommitmentPoolProps } from '@utils/types';
-import { Box, HStack, Text, VStack } from '@chakra-ui/react';
-import Link from 'next/link';
-import { PoolListItem } from '@components/PoolListItem';
+import type { GetStaticProps, NextPage } from "next";
+import styles from "@styles/Home.module.css";
+import prisma from "@utils/prisma";
+import { CommitmentPoolProps } from "@utils/types";
+import { Box, HStack, Text, VStack } from "@chakra-ui/react";
+import Link from "next/link";
+import { PoolListItem } from "@components/PoolListItem";
 
 type Props = {
-  pools: CommitmentPoolProps[]
-}
-
-const Home: NextPage<Props> = ({ pools }) => {
-  return (
-    <Box className={styles.container}>
-      <Box className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to Power in Numbers!
-        </h1>
-        <Text className={styles.description}>Commitment Pools</Text>
-        <VStack maxHeight='500px' overflow="scroll">
-          {pools.map(
-            (pool, idx) => <PoolListItem key={idx} pool={pool} />)
-          }
-        </VStack>
-      </Box>
-    </Box>
-  )
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const pools = await prisma.commitmentPool.findMany({
-  });
-  return {
-    props: { pools: JSON.parse(JSON.stringify(pools)) },
-    revalidate: 10,
-  };
+    pools: CommitmentPoolProps[];
 };
 
-export default Home
+const Home: NextPage<Props> = ({ pools }) => {
+    return (
+        <Box className={styles.container}>
+            <Box className={styles.main}>
+                <h1 className={styles.title}>Welcome to Power in Numbers!</h1>
+                <Text className={styles.description}>Commitment Pools</Text>
+                <VStack maxHeight="500px" overflow="scroll">
+                    {pools.map((pool, idx) => (
+                        <PoolListItem key={idx} pool={pool} />
+                    ))}
+                </VStack>
+            </Box>
+        </Box>
+    );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+    const pools = await prisma.commitmentPool.findMany({});
+    return {
+        props: { pools: JSON.parse(JSON.stringify(pools)) },
+        revalidate: 10,
+    };
+};
+
+export default Home;
