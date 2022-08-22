b10a135887f79d86099bbe5bf596a89dd9872cc4
straightupjac <jdc.jaclyn@gmail.com>
2022-08-22 00:06:26 -0700

    create commitment pool form validation

--- pages/pool/create.tsx
+++ pages/pool/create.tsx
@@ -1,17 +1,36 @@
-import { Box, Button, Input, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, HStack, IconButton, Input, Text, Textarea, VStack } from '@chakra-ui/react';
 import styles from '@styles/Home.module.css'
-import { generateNewKeyPair, generateNewKeyPairHex } from '@utils/crypto';
-import { generateKeyPairSync } from 'crypto';
+import { generateNewKeyPair } from '@utils/crypto';
+import * as Yup from 'yup';
 import { useFormik } from 'formik';
 import { NextPage } from 'next';
+import { useState } from 'react';
+import { KeyPair } from '@utils/types';
+import { useClipboard } from '@chakra-ui/react'
+import { CheckIcon, CopyIcon } from '@chakra-ui/icons';
 
 const CreatePool: NextPage = ({ }) => {
-  const formik = useFormik({
+  const [keyPair, setKeyPair] = useState<KeyPair>({});
+  const [submitted, setSubmitted] = useState<boolean>(false);
+  const { hasCopied, onCopy } = useClipboard(keyPair.privateKey || '');
+
+  // validation schema
+  const createPoolSchema = Yup.object().shape({
+    title: Yup.string()
+      .min(2, 'Too Short!')
+      .max(80, 'Too Long!')
+      .required('Required'),
+    description: Yup.string()
+      .min(2, 'Too Short!')
+      .max(800, 'Too Long!')
+  });
+
+  const createPoolForm = useFormik({
     initialValues: {
       title: '',
       description: '',
     },
-    // TODO: add validation
+    validationSchema: createPoolSchema,
     onSubmit: (values) => {
       onSubmit(values);
     },
@@ -19,8 +38,13 @@ const CreatePool: NextPage = ({ }) => {
 
   const onSubmit = async ({ title, description }: { title: string, description: string }) => {
     const { privateKey, publicKey } = await generateNewKeyPair();
-    console.log(privateKey, publicKey);
-    alert(JSON.stringify({ title, description }));
+    setKeyPair({ privateKey, publicKey })
+    setSubmitted(true);
+  }
+
+  const onClickDone = () => {
+    setSubmitted(false);
+    createPoolForm.resetForm();
   }
 
   return (
@@ -29,24 +53,53 @@ const CreatePool: NextPage = ({ }) => {
         <h1 className={styles.title}>
           Create pool
         </h1>
-        <form onSubmit={formik.handleSubmit}>
+        <form onSubmit={createPoolForm.handleSubmit}>
           <label htmlFor='title'>Title</label>
+          {createPoolForm.errors.title &&
+            <Text color="red.400">*{createPoolForm.errors.title}</Text>
+          }
           <Input
             id='title'
             name='title'
             type='title'
-            onChange={formik.handleChange}
-            value={formik.values.title}
+            onChange={createPoolForm.handleChange}
+            value={createPoolForm.values.title}
           />
           <label htmlFor='description'>Description</label>
-          <Input
+          {createPoolForm.errors.description && <Text color="red.400">*{createPoolForm.errors.description}</Text>}
+          <Textarea
             id='description'
             name='description'
-            type='description'
-            onChange={formik.handleChange}
-            value={formik.values.description}
+            onChange={createPoolForm.handleChange}
+            value={createPoolForm.values.description}
           />
-          <Button type='submit' marginTop={4}>Submit</Button>
+          {!submitted && <Button type='submit' marginTop={4}>Submit</Button>}
+          {submitted &&
+            <Box
+              background='blue.50'
+              padding={4} marginTop={4}
+              marginBottom={4} borderRadius={8}
+            >
+              <VStack gap={2} width={'100%'}>
+                <Text>Operator Public Key</Text>
+                <Flex mb={2} width={'100%'}>
+                  <Input value={keyPair.publicKey} isReadOnly placeholder='Welcome' />
+                  {hasCopied ?
+                    <IconButton aria-label={'completed copy button'} ml={2} icon={<CheckIcon />} /> :
+                    <IconButton onClick={onCopy} ml={2} aria-label={'copy icon button'} icon={<CopyIcon />} />}
+                </Flex>
+                <Text>Operator Private Key</Text>
+                <Flex mb={2} width={'100%'}>
+                  <Input value={keyPair.privateKey} isReadOnly placeholder='Welcome' />
+                  {hasCopied ?
+                    <IconButton aria-label={'completed copy button'} ml={2} icon={<CheckIcon />} /> :
+                    <IconButton onClick={onCopy} ml={2} aria-label={'copy icon button'} icon={<CopyIcon />} />}
+                </Flex>
+                <Flex justifyContent="end" width='100%'>
+                  <Button onClick={onClickDone}>Done</Button>
+                </Flex>
+              </VStack>
+            </Box>}
         </form>
       </Box>
     </Box>
