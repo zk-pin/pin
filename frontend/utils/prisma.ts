9ee986668ad0d9fc44f572ae8298e2f71565a20e
straightupjac <jdc.jaclyn@gmail.com>
2022-08-27 12:24:41 -0700

    prisma config

--- frontend/utils/prisma.ts
+++ frontend/utils/prisma.ts
@@ -7,9 +7,28 @@ if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    global.prisma = new PrismaClient({ log: ["query"] });
   }
   prisma = global.prisma;
 }
 
 export default prisma;
+
+// recommended config by prisma docs, but next build fails
+/*
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
+declare global {
+  // allow global `var` declarations
+  // @ts-ignore eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export default prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+*/
