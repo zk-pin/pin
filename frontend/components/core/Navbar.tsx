2c5901b9df82ce2cf4fdf800cdedf24c8f9770dd
amirgamil <amir.amgad01@gmail.com>
2022-08-29 18:58:53 -0400

    chore: fix more bugs w. new privkey gen

--- frontend/components/core/Navbar.tsx
+++ frontend/components/core/Navbar.tsx
@@ -1,8 +1,15 @@
-import { Button, Flex, HStack, Spinner, Text, useToast } from "@chakra-ui/react"
-import { useRouter } from 'next/router';
-import { signIn, signOut, useSession } from 'next-auth/react';
-import Link from 'next/link';
-import { Image as ChakraImage } from '@chakra-ui/react'
+import {
+  Button,
+  Flex,
+  HStack,
+  Spinner,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { Image as ChakraImage } from "@chakra-ui/react";
 import { useLiveQuery } from "dexie-react-hooks";
 import { checkCachedSignerData } from "@utils/api";
 import { useEffect, useRef, useState } from "react";
@@ -19,70 +26,83 @@ export const NavBar = () => {
 
   const { data: session, status } = useSession();
 
-  const cachedSigner = useLiveQuery(
-    async () => {
-      if (!session) { return; }
-      // @ts-ignore TODO:
-      const signerData = await getCachedSignerData(session.user.id);
-      loadingCachedSigned.current = false;
-      return signerData;
-    }, [session, session?.user]);
+  const cachedSigner = useLiveQuery(async () => {
+    if (!session) {
+      return;
+    }
+    // @ts-ignore TODO:
+    const signerData = await getCachedSignerData(session.user.id);
+    loadingCachedSigned.current = false;
+    return signerData;
+  }, [session, session?.user]);
 
   const signInTwitter = () => {
     signIn();
-  }
+  };
+
+  const refreshData = () => {
+    // TODO: move to helper
+    router.replace(router.asPath);
+  };
 
   useEffect(() => {
-    if (loadingCachedSigned.current) { return; }
+    if (loadingCachedSigned.current) {
+      return;
+    }
     if (session) {
-      checkCachedSignerData(cachedSigner, session, toast);
+      const newKeyIssued = checkCachedSignerData(cachedSigner, session, toast);
+      if (newKeyIssued) {
+        refreshData();
+      }
     }
   }, [session]);
 
-  return (<Flex
-    as="header"
-    position="fixed"
-    alignContent="space-evenly"
-    justifyContent="space-between"
-    w="100%"
-    p={2}
-    px={4}
-    backdropFilter="saturate(150%) blur(20px)"
-    zIndex={100}
-  >
-    <Link href="/">
-      <Button size="lg">
-        Home
-      </Button>
-    </Link>
-    {status === 'loading' &&
-      <Button size="lg" rightIcon={<Spinner />}>
-        Login
-      </Button>
-    }
-    {!session && status !== 'loading' &&
-      <Button size="lg" onClick={() => signInTwitter()} isActive={isActive('/signup')}>
-        Login
-      </Button>
-    }
-    {session &&
-      <HStack>
-        <HStack>
-          <ChakraImage
-            alt={`${session.user?.name}'s profile picture`}
-            src={session.user?.image || ''}
-            width={10}
-            height={10}
-            borderRadius={'50%'}
-          />
-          <Text fontWeight="bold">
-            Hi {session?.user?.name}
-          </Text>
-        </HStack>
-        <Button size="lg" variant="ghost" onClick={() => signOut()}>
-          Log out
+  return (
+    <Flex
+      as="header"
+      position="fixed"
+      alignContent="space-evenly"
+      justifyContent="space-between"
+      w="100%"
+      p={2}
+      px={4}
+      backdropFilter="saturate(150%) blur(20px)"
+      zIndex={100}
+    >
+      <Link href="/">
+        <Button size="lg">Home</Button>
+      </Link>
+      {status === "loading" && (
+        <Button size="lg" rightIcon={<Spinner />}>
+          Login
         </Button>
-      </HStack>
-    }
-  </Flex>)
-}
+      )}
+      {!session && status !== "loading" && (
+        <Button
+          size="lg"
+          onClick={() => signInTwitter()}
+          isActive={isActive("/signup")}
+        >
+          Login
+        </Button>
+      )}
+      {session && (
+        <HStack>
+          <HStack>
+            <ChakraImage
+              alt={`${session.user?.name}'s profile picture`}
+              src={session.user?.image || ""}
+              width={10}
+              height={10}
+              borderRadius={"50%"}
+            />
+            <Text fontWeight="bold">Hi {session?.user?.name}</Text>
+          </HStack>
+          <Button size="lg" variant="ghost" onClick={() => signOut()}>
+            Log out
+          </Button>
+        </HStack>
+      )}
+    </Flex>
+  );
+};
