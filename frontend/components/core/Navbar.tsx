eb8f0583921f73090734fe3459cb1ccd20f85cf4
amirgamil <amir.amgad01@gmail.com>
2022-08-31 00:38:25 -0400

    chore: return of the logs

--- frontend/components/core/Navbar.tsx
+++ frontend/components/core/Navbar.tsx
@@ -25,8 +25,10 @@ export const NavBar = () => {
 
   const { data: session, status } = useSession();
 
+  console.log("session nav: ", session);
   const cachedSigner = useLiveQuery(async () => {
     if (!session) {
+      console.log("no session");
       return;
     }
 
@@ -35,6 +37,7 @@ export const NavBar = () => {
 
     //initial load, cache has not yet been created for this user.id
     if (!signerData && loadingCachedSigned.current) {
+      console.log("first use live query setting the cache");
       await checkCachedSignerData(signerData, session, toast);
     }
 
@@ -52,9 +55,14 @@ export const NavBar = () => {
 
   useEffect(() => {
     if (loadingCachedSigned.current) {
+      console.log("current is loading");
       return;
     }
     if (session) {
+      console.log(
+        "session is valid and loading cache in useEffect: ",
+        loadingCachedSigned.current
+      );
       const newKeyIssued = checkCachedSignerData(cachedSigner, session, toast);
       if (newKeyIssued) {
         refreshData();
